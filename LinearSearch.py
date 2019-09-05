eb38944b09abea24fc08114b8b888efbdc72fd2d
DMHarish <52617695+DMHarish@users.noreply.github.com>
2019-09-06 03:30:32 +0530

    Update LinearSearch.py

--- LinearSearch.py
+++ LinearSearch.py
@@ -3,18 +3,16 @@
 numbers. We use for loop to iterate through the list and then we print the
 True or False if the algorithm finds that number and when it doesnot respectively'''
 import random
-# data = Put that in a variable`
 data = [1,2,3,4,5,6]
 
-target = int(input('Enter the number between 0 to 10 '))
 def linear_search(data,target): #Enter the two necessary parameters
-    for i in data: #The two brackets denotes two missing function
-        if i == target:
+    for i in range(len(data)): #The two brackets denotes two missing function
+        if data[i] == target:
             return True
         else:
             return False
 
-
+target=random.randrange(1,10)
 print(data)
 print(target)
 a=linear_search(data,target)
